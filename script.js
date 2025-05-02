1220c15ada2ec05bb05d8e28145e1497a246574b
BrunaMeili <brunameiliface@hotmail.com>
2025-05-02 15:17:39 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const botao = document.getElementById('botao');
-const paragrafo = document.getElementById('paragrafo');
-
-botao.addEventListener('click', function() {
-  paragrafo.textContent = '"Eu acredito que às vezes são as pessoas que ninguém espera nada que fazem as coisas que ninguém consegue imaginar." - Alan Turing';
-});
